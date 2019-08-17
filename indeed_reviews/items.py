5f9ecb9b979393e17e0e757e18e30d685e704a6e
wim86 <wimhermans2@hotmail.com>
2019-08-17 23:30:23 +0300

    added itemloader

--- indeed_reviews/items.py
+++ indeed_reviews/items.py
@@ -6,6 +6,15 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader.processors import  TakeFirst, Identity
+
+class IndeedReviewsItemLoader(scrapy.loader.ItemLoader):
+    """ Class containing the ItemLoader for clothes"""
+    # default input & output processors
+    # will be executed for each item loaded,
+    # except if a specific in or output processor is specified
+    default_input_processor = Identity()
+    default_output_processor = TakeFirst()
 
 
 class IndeedReviewsItem(scrapy.Item):
@@ -17,4 +26,6 @@ class IndeedReviewsItem(scrapy.Item):
     location = scrapy.Field()
     date = scrapy.Field()
     review = scrapy.Field()
+    pros = scrapy.Field()
+    cons = scrapy.Field()
     pass
